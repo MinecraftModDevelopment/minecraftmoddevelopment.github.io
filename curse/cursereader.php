bf886212e6e02b97587a268b8b1c4a6ac83daf31
TheCodedOne <dr.kcodex@gmail.com>
2017-01-08 15:31:49 +0000

    reorientate

--- curse/cursereader.php
+++ curse/cursereader.php
@@ -3,10 +3,12 @@ include 'html_dom.php';
 $html = file_get_html('https://minecraft.curseforge.com/projects/'.$_GET['c']);
 $json = array();
 $arz=$html->find('div.info-data');
+$json['name']=(string)$html->find('span.overflow-tip')[0];
+$json['desc']=str_replace("\"","\\\"",$html->find('meta[property=og:description]')[0]->content);
+$json['icon']=$html->find('a.lightbox')[0]->href;	
 $json['download']=(string)str_replace(array(",","\n"), "", $arz[2]->plaintext);
 $json['created']=$arz[0]->plaintext;
-$json['updated']=$arz[1]->plaintext;
-$json['name']=(string)$html->find('span.overflow-tip')[0];
+$json['lastUpdated']=$arz[1]->plaintext;
 $categories = array();
 $i = 0;
 foreach($html->find('a.e-avatar32') as $element) {
@@ -25,6 +27,4 @@ foreach($html->find('div.avatar-wrapper') as $element) {
 }
 $json['categories']=$categories;
 $json['authors']=$authors;
-$json['desc']=str_replace("\"","\\\"",$html->find('meta[property=og:description]')[0]->content);
-$json['icon']=$html->find('a.lightbox')[0]->href;
 echo str_replace(array("<\/div>","<\/span>",'<span class=\"overflow-tip\">','<div class=\"info-data\">'),"",json_encode($json,JSON_PRETTY_PRINT));
