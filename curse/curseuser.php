cb5d8f4a93ba750b0d9303d55b0e5be3069d95a7
TheCodedOne <dr.kcodex@gmail.com>
2017-01-21 13:40:57 +0000

    bep

--- curse/curseuser.php
+++ curse/curseuser.php
@@ -7,13 +7,13 @@ $dup = 0;
 for ($x = 1; $x <= 100; $x++) {
 		$html = file_get_html('https://minecraft.curseforge.com/members/'.$_GET['c'].'/projects?page='.$x);
 		foreach($html->find('a.e-avatar64') as $element) {
-			$projectJson = json_decode(file_get_html('http://mcmoddev.com/curse/cursereader.php?c='.str_replace('/projects/','',$element->href)));
 			if(!array_key_exists(str_replace('/projects/','',$element->href),$json)) {
-				$json[str_replace('/projects/','',$element->href)]=$projectJson;
+				$json[$index]=str_replace('/projects/','',$element->href);
 			} else {
 				$dup = 1;
 				break;
 			}
+			$index++;
 		}
 		if($dup==1)
 			break;
