b3dd183ef6c26ce69c50254e960dcbb7b3aabb2d
TheCodedOne <dr.kcodex@gmail.com>
2017-01-21 13:31:40 +0000

    bleh

--- curse/curseuser.php
+++ curse/curseuser.php
@@ -5,7 +5,7 @@ $lastPg = 0;
 for ($x = 0; $x <= 10; $x++) {
 	$html = file_get_html('https://minecraft.curseforge.com/members/'.$_GET['c'].'/projects?page='.$x);
 		foreach($html->find('a.e-avatar64') as $element) {
-			$projectJson = file_get_html('http://mcmoddev.com/curse/cursereader.php?c='.str_replace('/projects/','',$element->href);
+			$projectJson = file_get_html('http://mcmoddev.com/curse/cursereader.php?c='.str_replace('/projects/','',$element->href));
 			echo str_replace('/projects/','',$element->href) . '<br>';
 		}
 	$lastPg=$x;
