2a81c905126070aa1ec0f5d658ce3526839d9ce7
TheCodedOne <dr.kcodex@gmail.com>
2017-01-21 13:25:26 +0000

    test

--- curse/curseuser.php
+++ curse/curseuser.php
@@ -1,7 +1,11 @@
 <?php
 include 'html_dom.php';
-$html = file_get_html('https://minecraft.curseforge.com/members/'.$_GET['c'].'/projects');
 $json = array();
-
-foreach($html->find('a.e-avatar64') as $element) 
-       echo $element->href . '<br>';
+$lastPg = 0;
+for ($x = 0; $x <= 10; $x++) {
+	$html = file_get_html('https://minecraft.curseforge.com/members/'.$_GET['c'].'/projects?page='.$x);
+		foreach($html->find('a.e-avatar64') as $element) {
+			echo $element->href . '<br>';
+		}
+	$lastPg=$x;
+} 
